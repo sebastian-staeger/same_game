a17965729eb8b5ab7304aa576a0a50f36e9bdef9
Sebastian Stäger <sebastian.staeger@gmx.de>
2021-06-19 12:50:09 +0200

    while loop game

--- main.cpp
+++ main.cpp
@@ -5,38 +5,33 @@
 #include <time.h>
 #include <tuple>
 
+
 using namespace std;
 
 std::tuple<int, int> zugeingabe(string zug);
-
 
 vector<vector<string>> search_and_color(vector<vector<string>> &feld);
 
 vector<vector<string>> group(int i, int j, vector<vector<string>> &marked, const vector<vector<string>> &feld);
 
-int main() {
+vector<vector<string>> nachrutschen_spalte(vector<vector<string>> &feld);
 
-    /*std::cout << "\033[31;41;4m___\033[0m" << " ";
-    std::cout << "\033[32;42;4m___\033[0m" << " ";
-    std::cout << "\033[33;43;4m___\033[0m" << " ";
-    std::cout << "\033[34;44;4m___\033[0m" << " ";
-    std::cout << "\033[37;47;4m___\033[0m" << " " << endl;
-*/
+int main() {
+/*
     vector<vector<string> > spielfeld(9, vector<string>(9));
     vector<vector<string> > marked(9, vector<string>(9));
     vector<string> farben{"\033[31;41;4m___\033[0m", "\033[32;42;4m___\033[0m", "\033[33;43;4m___\033[0m",
                           "\033[34;44;4m___\033[0m", "\033[35;45;4m___\033[0m"};
 
     //srand(time(NULL));
-    srand(11);
+    srand(7);
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
             int v1 = rand() % 5;
             spielfeld[i][j] = farben[v1];
-            //cout << farben[v1] << "wurde an stelle " << i << " " << j << "eingefügt" << endl;
-            //cout << spielfeld[i][j] << endl;
         }
     }
+    //KOPIE VOM SPIELFELD ANLEGEN
     marked = spielfeld;
 
     for (int i = 0; i < spielfeld.size(); i++) {
@@ -48,13 +43,13 @@ int main() {
     }
     cout << " A " << " B " << " C " << " D " << " E " << " F " << " G " << " H " << " I " << endl;
 
-    /* DEBUG zugeingabe
+     DEBUG zugeingabe
     auto zug = zugeingabe("d2");
     cout << std::get<0>(zug) << std::get<1>(zug) << endl;
-
     cout << spielfeld[std::get<0>(zug)][std::get<1>(zug)] << endl;
-*/
 
+
+    // FINDE ALLE STEINE DIE TEIL IRGENDEINER GRUPPE SIND UND FÄRBE SIE WEISS EIN
     vector<vector<string>> haha = search_and_color(spielfeld);
 
     for (int i = 0; i < haha.size(); i++) {
@@ -65,7 +60,9 @@ int main() {
         cout << " " << i + 1 << endl;
     }
     cout << endl;
-    auto zug = zugeingabe("e5");
+    auto zug = zugeingabe("c8");
+
+    // GRUPPE FINDEN UND SCHWARZ EINFÄRBEN
     vector<vector<string>> hehe = group(std::get<0>(zug), std::get<1>(zug), marked, spielfeld);
 
     for (int i = 0; i < hehe.size(); i++) {
@@ -74,6 +71,96 @@ int main() {
             cout << hehe[i][j];
         }
         cout << " " << i + 1 << endl;
+    }
+    cout << endl;
+
+    // NACHRUTSCHEN
+    vector<vector<string>> pushed_up = nachrutschen_spalte(hehe);
+
+    for (int i = 0; i < pushed_up.size(); i++) {
+
+        for (int j = 0; j < pushed_up[i].size(); j++) {
+            cout << pushed_up[i][j];
+        }
+        cout << " " << i + 1 << endl;
+    }
+    cout << endl;
+    //KOPIE VOM SPIELFELD ANLEGEN
+    vector<vector<string> > NAECHSTEMARKE(9, vector<string>(9));
+    NAECHSTEMARKE = pushed_up;
+
+    vector<vector<string>> NAECHSTERUNDE = group(std::get<0>(zug), std::get<1>(zug), NAECHSTEMARKE, pushed_up);
+
+    for (int i = 0; i < NAECHSTERUNDE.size(); i++) {
+
+        for (int j = 0; j < NAECHSTERUNDE[i].size(); j++) {
+            cout << NAECHSTERUNDE[i][j];
+        }
+        cout << " " << i + 1 << endl;
+    }
+
+    cout << endl;
+    vector<vector<string>> pushed_up1 = nachrutschen_spalte(NAECHSTERUNDE);
+
+    for (int i = 0; i < pushed_up1.size(); i++) {
+
+        for (int j = 0; j < pushed_up1[i].size(); j++) {
+            cout << pushed_up1[i][j];
+        }
+        cout << " " << i + 1 << endl;
+    }
+
+
+*/
+    // implementation
+    vector<vector<string> > game_board(9, vector<string>(9));
+    vector<vector<string> > copy_game_board(9, vector<string>(9));
+    vector<string> colors{"\033[31;41;4m___\033[0m", "\033[32;42;4m___\033[0m", "\033[33;43;4m___\033[0m",
+                          "\033[34;44;4m___\033[0m", "\033[35;45;4m___\033[0m"};
+
+    // set colors
+    //srand(time(NULL));
+    srand(7);
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            int c = rand() % 5;
+            game_board[i][j] = colors[c];
+        }
+    }
+
+    for (int i = 0; i < game_board.size(); i++) {
+
+        for (int j = 0; j < game_board[i].size(); j++) {
+            cout << game_board[i][j];
+        }
+        cout << " " << i + 1 << endl;
+    }
+    cout << " A " << " B " << " C " << " D " << " E " << " F " << " G " << " H " << " I " << endl;
+
+
+    int counter = 0;
+    while(counter < 10){
+
+        copy_game_board = game_board;
+
+        cout << "Geben sie ihren zug ein (z. B. 'f3')";
+        string move_input;
+        cin >> move_input;
+        auto move = zugeingabe(move_input);
+
+        game_board = group(std::get<0>(move), std::get<1>(move), copy_game_board, game_board);
+
+        game_board = nachrutschen_spalte(game_board);
+
+        for (int i = 0; i < game_board.size(); i++) {
+
+            for (int j = 0; j < game_board[i].size(); j++) {
+                cout << game_board[i][j];
+            }
+            cout << " " << i + 1 << endl;
+        }
+        cout << " A " << " B " << " C " << " D " << " E " << " F " << " G " << " H " << " I " << endl;
+        counter++;
     }
 
     return 0;
@@ -88,24 +175,26 @@ std::tuple<int, int> zugeingabe(string zug) {
     int spalte;
 
     if (row == '1') zeile = 0;
-    if (row == '2') zeile = 1;
-    if (row == '3') zeile = 2;
-    if (row == '4') zeile = 3;
-    if (row == '5') zeile = 4;
-    if (row == '6') zeile = 5;
-    if (row == '7') zeile = 6;
-    if (row == '8') zeile = 7;
-    if (row == '9') zeile = 8;
+    else if (row == '2') zeile = 1;
+    else if (row == '3') zeile = 2;
+    else if (row == '4') zeile = 3;
+    else if (row == '5') zeile = 4;
+    else if (row == '6') zeile = 5;
+    else if (row == '7') zeile = 6;
+    else if (row == '8') zeile = 7;
+    else if (row == '9') zeile = 8;
+    else cout << "Illegal move" << endl;
 
     if (col == 'a') spalte = 0;
-    if (col == 'b') spalte = 1;
-    if (col == 'c') spalte = 2;
-    if (col == 'd') spalte = 3;
-    if (col == 'e') spalte = 4;
-    if (col == 'f') spalte = 5;
-    if (col == 'g') spalte = 6;
-    if (col == 'h') spalte = 7;
-    if (col == 'i') spalte = 8;
+    else if (col == 'b') spalte = 1;
+    else if (col == 'c') spalte = 2;
+    else if (col == 'd') spalte = 3;
+    else if (col == 'e') spalte = 4;
+    else if (col == 'f') spalte = 5;
+    else if (col == 'g') spalte = 6;
+    else if (col == 'h') spalte = 7;
+    else if (col == 'i') spalte = 8;
+    else cout << "Illegal move" << endl;
 
     return std::make_tuple(zeile, spalte);
 }
@@ -343,6 +432,17 @@ vector<vector<string>> group(int i, int j, vector<vector<string>> &marked, const
     return marked;
 }
 
-void has_neighbour(vector<vector<string>> &feld) {
-
+vector<vector<string>> nachrutschen_spalte(vector<vector<string>> &feld) {
+    for (int k = 0; k < 9; k++) {
+        for (int i = feld.size() - 1; i > 0; i--) {
+            for (int j = feld.size() - 1; j >= 0; j--) {
+                if (feld[i][j] == "\033[30;40;4m___\033[0m" && feld[i - 1][j] != "\033[30;40;4m___\033[0m") {
+                    feld[i][j] = feld[i - 1][j];
+                    feld[i - 1][j] = "\033[30;40;4m___\033[0m";
+                }
+            }
+        }
+    }
+    return feld;
 }
+
