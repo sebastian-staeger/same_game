4449ff013966e0dee6003c0133d848e9e8e22606
Sebastian Stäger <sebastian.staeger@gmx.de>
2021-06-20 10:10:31 +0200

    implemented score, only thing missing is gap closing when one column is empty

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ vector<vector<string>> nachrutschen_spalte(vector<vector<string>> &feld);
 
 bool game_over(vector<vector<string>> &white_spot_field);
 
-int count_score(vector<vector<string>> &board_before_move, vector<vector<string>> &board_after_move);
+int count_score(vector<vector<string>> &game_board);
 
 int main() {
 /*
@@ -126,7 +126,9 @@ int main() {
     vector<vector<string> > white_board(9, vector<string>(9));
     vector<string> colors{"\033[31;41;4m___\033[0m", "\033[32;42;4m___\033[0m", "\033[33;43;4m___\033[0m",
                           "\033[34;44;4m___\033[0m", "\033[35;45;4m___\033[0m"};
-    int score = 0;
+    int score_total = 0;
+    int score_after = 0;
+    int score_before = 0;
 
     // set colors
     //srand(time(NULL));
@@ -155,11 +157,17 @@ int main() {
         cin >> move_input;
         auto move = zugeingabe(move_input);
 
+        score_before = count_score(game_board);
+
         game_board = group(std::get<0>(move), std::get<1>(move), copy_game_board, game_board);
 
         game_board = nachrutschen_spalte(game_board);
 
         white_board = search_and_color(game_board);
+
+        score_after = count_score(game_board);
+        score_after = (score_after - score_before) * (score_after - score_before - 1);
+        score_total += score_after;
 
         for (int i = 0; i < game_board.size(); i++) {
 
@@ -169,6 +177,11 @@ int main() {
             cout << " " << i + 1 << endl;
         }
         cout << " A " << " B " << " C " << " D " << " E " << " F " << " G " << " H " << " I " << endl;
+
+        cout << "SCORE: "<< score_total << endl;
+
+
+
 
     } while (!game_over(white_board));
 
@@ -233,27 +246,27 @@ vector<vector<string>> search_and_color(vector<vector<string>> &feld) {
                 }
             } else if (i == 0 && j < 8 && j > 0) {
                 if ((feld[i][j] == feld[i + 1][j] || feld[i][j] == feld[i][j - 1] ||
-                    feld[i][j] == feld[i][j + 1]) && feld[i][j] != BLACK) {
+                     feld[i][j] == feld[i][j + 1]) && feld[i][j] != BLACK) {
                     white_spots[i][j] = WHITE;
                 }
             } else if (j == 0 && i < 8 && i > 0) {
                 if ((feld[i][j] == feld[i + 1][j] || feld[i][j] == feld[i][j + 1] ||
-                    feld[i][j] == feld[i - 1][j]) && feld[i][j] != BLACK) {
+                     feld[i][j] == feld[i - 1][j]) && feld[i][j] != BLACK) {
                     white_spots[i][j] = WHITE;
                 }
             } else if (i == 8 && j < 8 && j > 0) {
                 if ((feld[i][j] == feld[i - 1][j] || feld[i][j] == feld[i][j + 1] ||
-                    feld[i][j] == feld[i][j - 1]) && feld[i][j] != BLACK) {
+                     feld[i][j] == feld[i][j - 1]) && feld[i][j] != BLACK) {
                     white_spots[i][j] = WHITE;
                 }
             } else if (j == 8 && i < 8 && i > 0) {
                 if ((feld[i][j] == feld[i + 1][j] || feld[i][j] == feld[i][j - 1] ||
-                    feld[i][j] == feld[i - 1][j]) && feld[i][j] != BLACK) {
+                     feld[i][j] == feld[i - 1][j]) && feld[i][j] != BLACK) {
                     white_spots[i][j] = WHITE;
                 }
             } else {
                 if ((feld[i][j] == feld[i + 1][j] || feld[i][j] == feld[i][j - 1] || feld[i][j] == feld[i - 1][j] ||
-                    feld[i][j] == feld[i][j + 1]) && feld[i][j] != BLACK) {
+                     feld[i][j] == feld[i][j + 1]) && feld[i][j] != BLACK) {
                     white_spots[i][j] = WHITE;
                 }
             }
@@ -468,10 +481,14 @@ bool game_over(vector<vector<string>> &white_spot_field) {
     return true;
 }
 
-int count_score(vector<vector<string>> &board_before_move, vector<vector<string>> &board_after_move){
-    for (int i = 0; i < board_before_move.size(); i++) {
-        for (int j = 0; j < board_before_move[i].size(); j++) {
+int count_score(vector<vector<string>> &game_board) {
+    int score = 0;
+
+    for (int i = 0; i < game_board.size(); i++) {
+        for (int j = 0; j < game_board[i].size(); j++) {
+            if (game_board[i][j] == BLACK) score++;
 
         }
     }
+    return score;
 }
